6b05213af8bcb60b3141a627221d82aecd1297b8
Feng <zhufeng1980@hotmail.com>
2021-07-09 07:07:55 +1200

    End of section 10.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -18,7 +18,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,28 @@
+import { ShopService } from 'src/app/shop/shop.service';
+import { IProduct } from 'src/app/shared/models/product';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
+
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')!).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
