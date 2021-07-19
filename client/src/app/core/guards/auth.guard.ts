30c45065cd61866e454c84c0a3850266f4f30623
Feng <zhufeng1980@hotmail.com>
2021-07-20 07:56:09 +1200

    End of section 17

---
+++ client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { AccountService } from 'src/app/account/account.service';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<any> {
+    return this.accountService.currentUser$.pipe(
+      // map(auth => {
+      //   if (auth) return true;
+      //   this.router.navigate(['account/login'], {queryParams: {returnUrl: state.url}});
+      // })
+      map(auth => {
+        if (auth) {
+          return true;
+        }
+        this.router.navigate(['account/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+      })
+    );
+  }
+  
+}
